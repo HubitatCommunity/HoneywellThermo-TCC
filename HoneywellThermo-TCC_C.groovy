0613adde50edcc863a803e04a098bb9e3fa7133a
c steele <csteele@pandoradigital.com>
2020-11-07 09:58:30 -0800

    v1.3.12

--- HoneywellThermo-TCC_C.groovy
+++ HoneywellThermo-TCC_C.groovy
@@ -66,7 +66,7 @@
  *
 */
 
- public static String version()     {  return "v1.3.11"  }
+ public static String version()     {  return "v1.3.12"  }
  public static String tccSite() 	{  return "www.mytotalconnectcomfort.com"  }
 
 metadata {
